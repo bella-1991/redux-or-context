959493320b1732710f388ebec6bcaafc43779efc
bella-1991 <shalini.hansaka@gmail.com>
2020-06-02 14:06:08 +0530

    changes to header

--- src/components/global/header.js
+++ src/components/global/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header = () => {
     return (
         <header className="header">
-            <h1>Redux vs Context Api</h1>
+            <h1><a href="/">Redux vs Context Api</a></h1>
         </header>
     );
 }
--- src/components/redux/index.js
+++ src/components/redux/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Counter } from './components/counter';
+import { Main } from './components';
 import { store } from './store/configureStore'
 import './redux.css';
 
 export const ReduxWay = () => {
     return (
         <Provider store={store}>
-            <Counter />
+            <Main />
         </Provider>
     );
 }
